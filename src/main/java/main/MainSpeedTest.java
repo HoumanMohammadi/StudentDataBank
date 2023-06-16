f1595c17515b2415f8ee5cd2e10c21d4311054d8
Houman Mohammadi <houmane.el@gmail.com>
2023-06-16 16:24:06 +0200

    Speedtest implemented

---
+++ src/main/java/main/MainSpeedTest.java
@@ -0,0 +1,112 @@
+package main;
+
+import Animal.PassportPrintingService;
+import Animal.Pet;
+import Human.*;
+import org.openjdk.jmh.annotations.*;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@Warmup(iterations = 2, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Fork(1)
+public class MainSpeedTest {
+
+    public static void main(String[] args) throws IOException {
+        org.openjdk.jmh.Main.main(args);
+    }
+    @Benchmark
+    public static void run() {
+        StudentDB studentsArrayList= new StudentDB();
+        studentsArrayList.add(new Student("Houman", 1234));
+        studentsArrayList.add(new Student("Gerd", 12345));
+        studentsArrayList.add(new Student("Zied", 12346));
+
+
+
+        Student[] studentsArray = new Student[3];
+        studentsArray[0] = new Student("Houman", 1234);
+        studentsArray[1] = new Student("Gerd", 12345);
+        studentsArray[2] = new Student("Zied", 12346);
+
+        ComputerScienceStudent[] computerScienceStudent= new ComputerScienceStudent[3];
+        computerScienceStudent[0]= new ComputerScienceStudent("Houman", 1, true);
+        computerScienceStudent[1]= new ComputerScienceStudent("Natthias", 22, true);
+        computerScienceStudent[2]= new ComputerScienceStudent("Zied", 333, true);
+        StudentDB studentDataBank = new StudentDB(List.of(computerScienceStudent));
+        System.out.println(Arrays.toString(computerScienceStudent));
+
+        try {
+            System.out.println(studentDataBank.findById(22));
+        } catch (StudentNotFoundException e) {
+            System.out.println("ID nicht existiert");;
+        }
+        System.out.println("Es wird nichts ausgegeben werden");
+
+        for (int i=0; i< computerScienceStudent.length;i++){
+            PassportPrintingService<ComputerScienceStudent[]>  computerScienceStudentPassportPrintingService= new PassportPrintingService<>(computerScienceStudent);
+            computerScienceStudentPassportPrintingService.printPassport();
+        }
+
+
+        PassportPrintingService<Student> studentPassportPrintingService= new PassportPrintingService<>(studentsArray[2]);
+        studentPassportPrintingService.printPassport();
+
+        PassportPrintingService<Pet> petPassportPrintingService=new PassportPrintingService<>(new Pet("Batzi"));
+        petPassportPrintingService.printPassport();
+
+
+    }
+}/*
+}
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+     //   List<StudentDB> studentsArrayList=new StudentDB<>();
+        StudentDB studentsArrayList= new StudentDB();
+        studentsArrayList.add(new Student("Houman", 1234));
+        studentsArrayList.add(new Student("Gerd", 12345));
+        studentsArrayList.add(new Student("Zied", 12346));
+
+
+
+        Student[] studentsArray = new Student[3];
+        studentsArray[0] = new Student("Houman", 1234);
+        studentsArray[1] = new Student("Gerd", 12345);
+        studentsArray[2] = new Student("Zied", 12346);
+
+        ComputerScienceStudent[] computerScienceStudent= new ComputerScienceStudent[3];
+        computerScienceStudent[0]= new ComputerScienceStudent("Houman", 1, true);
+        computerScienceStudent[1]= new ComputerScienceStudent("Natthias", 22, true);
+        computerScienceStudent[2]= new ComputerScienceStudent("Zied", 333, true);
+        StudentDB studentDataBank = new StudentDB(List.of(computerScienceStudent));
+        System.out.println(Arrays.toString(computerScienceStudent));
+
+        try {
+            System.out.println(studentDataBank.findById(22));
+        } catch (StudentNotFoundException e) {
+            System.out.println("ID nicht existiert");;
+        }
+        System.out.println("Es wird nichts ausgegeben werden");
+
+        for (int i=0; i< computerScienceStudent.length;i++){
+            PassportPrintingService<ComputerScienceStudent[]>  computerScienceStudentPassportPrintingService= new PassportPrintingService<>(computerScienceStudent);
+            computerScienceStudentPassportPrintingService.printPassport();
+        }
+
+
+        PassportPrintingService<Student> studentPassportPrintingService= new PassportPrintingService<>(studentsArray[2]);
+        studentPassportPrintingService.printPassport();
+
+        PassportPrintingService<Pet> petPassportPrintingService=new PassportPrintingService<>(new Pet("Batzi"));
+        petPassportPrintingService.printPassport();
+
+
+    }
+
+*/
